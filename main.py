b69539d6b31228bc7aaba65602a68270ee4ba8fe
Artem Shirkalin <perecheslator@yandex.com>
2026-08-25 03:34:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ async def auth(request: Request, code: str | None = None, error: str | None = No
         return {"status": "Auth failed", "detail": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True) # reload можно убрать
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) # reload можно убрать
